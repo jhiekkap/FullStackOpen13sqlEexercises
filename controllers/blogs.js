c5547985889836277e1157ef1ade6a46ab8e395a
Jari Hiekkapelto <jari@rnd.works>
2023-06-27 14:06:30 +0300

    Tehtävä 13.13.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const { SECRET } = require('../util/config')
 const router = require('express').Router()
 const { Blog, User } = require('../models')
+const { Op } = require('sequelize')
 
 const tokenExtractor = (req, res, next) => {
   const authorization = req.get('authorization')
@@ -24,13 +25,22 @@ const blogFinder = async (req, _res, next) => {
   next()
 }
 
-router.get('/', async (_req, res) => {
+router.get('/', async (req, res) => {
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`
+    }
+  }
+
   const blogs = await Blog.findAll({
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where,
   })
   res.json(blogs)
 })
