5c9249a1b2178e7952fe891eb19e7abee374a031
Jari Hiekkapelto <jari@rnd.works>
2023-06-28 15:56:15 +0300

    add commands

--- commands.sql
+++ commands.sql
@@ -6,5 +6,11 @@
     likes int DEFAULT 0
 );
 
-insert into blogs (author, url, title,likes ) values ('Soini', 'http://timppa.fi', 'Ploki', 100);
-insert into blogs (author, url, title,likes ) values ('Helena', 'http://hellu.fi', 'OhoHupsis', 22);
+insert into users (username, name , created_at, updated_at) values('mluukkai@hki.fi', 'Matti Luukkainen', '2017-11-01 00:00:00', '2017-11-01 00:00:00');
+insert into users (username, name, created_at, updated_at ) values('kale@hki.fi', 'Kale', '2017-11-01 00:00:00', '2017-11-01 00:00:00');
+insert into users (username, name , created_at, updated_at) values('peku@hki.fi', 'Peku', '2017-11-01 00:00:00', '2017-11-01 00:00:00');
+
+insert into blogs (author, url, title,likes, user_id, year, created_at, updated_at) values ('Matti', 'http://timppa.fi', 'Ploki', 100, 1, 2017, '2017-11-01 00:00:00', '2017-11-01 00:00:00');
+insert into blogs (author, url, title,likes, user_id, year, created_at, updated_at) values ('Kale', 'http://hellu.fi', 'OhoHupsis', 22, 2, 2017, '2017-11-01 00:00:00', '2017-11-01 00:00:00');
+insert into blogs (author, url, title,likes, user_id, year, created_at, updated_at) values ('Peku', 'http://kukkuu.fi', 'Kukkuu', 0, 3, 2017, '2017-11-01 00:00:00', '2017-11-01 00:00:00');
+insert into blogs (author, url, title,likes, user_id, year, created_at, updated_at) values ('Matti', 'http://timppa.fi', 'Ploki2', 100,   1, 2018, '2017-11-01 00:00:00', '2017-11-01 00:00:00');
