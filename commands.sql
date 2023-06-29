4d3a62a4eceb8c240da56f7936def465a77703a8
Jari Hiekkapelto <jari@rnd.works>
2023-06-29 14:10:10 +0300

    Tehtävä 13.22.

--- commands.sql
+++ commands.sql
@@ -15,6 +15,7 @@ insert into blogs (author, url, title,likes, user_id, year, created_at, updated_
 insert into blogs (author, url, title,likes, user_id, year, created_at, updated_at) values ('Peku', 'http://kukkuu.fi', 'Kukkuu', 0, 3, 2017, '2017-11-01 00:00:00', '2017-11-01 00:00:00');
 insert into blogs (author, url, title,likes, user_id, year, created_at, updated_at) values ('Matti', 'http://timppa.fi', 'Ploki2', 100,   1, 2018, '2017-11-01 00:00:00', '2017-11-01 00:00:00');
 
-insert into readinglists (user_id, blog_id ) values (1, 1);
-insert into readinglists (user_id, blog_id ) values (2, 3);
-insert into readinglists (user_id, blog_id ) values (3, 4);
+insert into readinglists (user_id, blog_id, read ) values (1, 1, false);
+insert into readinglists (user_id, blog_id, read ) values (2, 3, false);
+insert into readinglists (user_id, blog_id, read ) values (3, 4, false);
+insert into readinglists (user_id, blog_id, read ) values (1, 3, true);
--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,9 +1,20 @@
 const router = require('express').Router()
 const { Readinglist } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res) => {
   const readinglist = await Readinglist.create(req.body)
   res.json(readinglist)
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const readinglist = await Readinglist.findByPk(req.params.id)
+  if (readinglist) {
+    await readinglist.update(req.body)
+    res.json(readinglist)
+  } else {
+    res.status(404).end()
+  }
 })
 
 module.exports = router
